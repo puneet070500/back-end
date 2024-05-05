0eb076c8c89a26e0218799ab4a4c23f7f5297531
Puneet Patel <Puneet070500@gmail.com>
2024-05-05 22:24:25 +0530

    ApiHandler

--- src/index.js
+++ src/index.js
@@ -4,4 +4,16 @@ import connectDB from "./db/db.js";
 dotenv.config();
 
 
-connectDB();
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () =>{
+        console.log(`Server running at port: ${process.env.PORT}`);
+    })
+    app.on("error", (err) => {
+        console.log(`Error: ${err}`);
+        throw err;
+    })
+})
+.catch((err) => {
+    console.log("Mongo Db connection failed !!!", err);
+});
